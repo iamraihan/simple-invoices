6f515ba5941c4224858d14c0b5ec68101413307c
iamraihan <trueraihan@gmail.com>
2022-02-12 21:43:26 +0600

    some code changes and final upload

--- app.js
+++ app.js
@@ -1,43 +1,91 @@
-//buyer details
-document.getElementById('detail-submit-btn').addEventListener('click', function () {
-    let buyerDetailsInput = document.getElementById('buyer-details-input')
-    let buyerInfo = document.getElementById('buyer-info').innerText = buyerDetailsInput.value
-})
-//item details 
-document.getElementById('add-details-btn').addEventListener('click', function () {
-    let infoTable = document.getElementById('info-table')
-    let itemNameInput = document.getElementById('item-name-input')
-    let itemPriceInput = document.getElementById('item-price-input')
-    let itemQuantityInput = document.getElementById('item-quantity-input')
+const detailsButton = document.getElementById("detail-submit-btn");
 
+detailsButton.addEventListener("click", function () {
+    const buyerDetails = document.getElementById("buyer-details-input");
+    document.getElementById("buyer-info").innerText = buyerDetails.value;
+    buyerDetails.value = "";
+});
 
-    let tr = document.createElement('tr')
-    let th = document.createElement('th')
+const addProductBtn = document.getElementById("add-details-btn");
 
-    let td1 = document.createElement('td')
-    let td2 = document.createElement('td')
-    let td3 = document.createElement('td')
+addProductBtn.addEventListener("click", function () {
+    const infoTable = document.getElementById("info-table");
+    const itemName = document.getElementById("item-name-input");
+    const itemPrice = document.getElementById("item-price-input");
+    const itemQuantity = document.getElementById("item-quantity-input");
 
-    let totalPrice = parseFloat(itemPriceInput) * parseFloat(itemQuantityInput)
+    if (
+        itemName.value == "" ||
+        itemPrice.value < 0 ||
+        itemQuantity.value < 0 ||
+        itemPrice.value == "" ||
+        itemQuantity.value == ""
+    ) {
+        console.log("sorry");
+        return;
+    }
 
-    th.innerText = itemNameInput.value
-    td1.innerText = itemPriceInput.value
-    td2.innerText = itemQuantityInput.value
-    td3.innerText = totalPrice.innerText
-
-    tr.appendChild(th)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
-
-    infoTable.appendChild(tr)
-
-
-
-    /*  <tr>
+    {
+        /* <tr>
      <th >1</th>
      <td>Mark</td>
-     <td>Otto</td>
-     <td>@mdo</td>
- </tr> */
-})
+    <td>Otto</td>
+    <td class='item-total'>@mdo</td>
+    </tr> */
+    }
+
+    const totalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value);
+
+    //   const tr = document.createElement("tr"); //<tr></tr>
+    const tr = element("tr");
+    const th = element("th");
+
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    const td3 = document.createElement("td");
+
+    td3.classList.add("item-total");
+    th.innerText = itemName.value; //<th >onion</th>
+    td1.innerText = itemPrice.value; //<th >1</th>
+    td2.innerText = itemQuantity.value;
+    td3.innerText = totalPrice;
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3); //<td class="item-total">35</td>
+    infoTable.appendChild(tr);
+    totalCalculation();
+});
+
+function element(param) {
+    return document.createElement(param);
+}
+
+function totalCalculation() {
+    const subTotal = calculateSubTotal();
+
+    const subTotalToDisplay = document.getElementById("sub-total");
+    subTotalToDisplay.innerText = subTotal;
+
+    const tax = subTotal * 0.2;
+
+    document.getElementById("tax").innerText = tax.toFixed(2);
+    document.getElementById("grand-total").innerText = subTotal + tax;
+    document.getElementById("grand-total-2").innerText = subTotal + tax;
+}
+
+function calculateSubTotal() {
+    let subTotal = 0;
+
+    const cost = document.getElementsByClassName("item-total");
+
+    for (let i = 0; i < cost.length; i++) {
+        const element = cost[i]; //<td class="item-total">35</td>
+        const price = parseInt(element.innerText);
+
+        subTotal = subTotal + price;
+    }
+
+    return subTotal;
+}
